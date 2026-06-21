ff7e8062aa790f175f448eb29f2ec4d2101e32f8
MaxMilu <MaxMilu@users.noreply.github.com>
2026-06-22 01:22:21 +0800

    fix: 添加缺失的 config.server.statusValues.stopped 翻译

--- src/renderer/pages/app/Server.vue
+++ src/renderer/pages/app/Server.vue
@@ -228,7 +228,7 @@ export default {
 
         addHearthbeatEndpoint(){
             const heartbeatMessage = this.$t('server.heartbeatSuccess')
-            this.$store.dispatch('server/addLog', this.$t('server.createHeartbeatEndpoint')
+            this.$store.dispatch('server/addLog', this.$t('server.createHeartbeatEndpoint'))
             this.host.router.get('/hb', function(request, response){
                 response.status(200).json({
                     remoteAddress: request.connection.remoteAddress,
